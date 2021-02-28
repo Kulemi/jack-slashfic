4271bf9f276eb68c7d5b69b4a23959c4b800bc2d
Kulemi <79752435+Kulemi@users.noreply.github.com>
2021-02-27 19:50:12 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,47 +1,43 @@
-import os
+import os, json
 from random import choice, randint
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
-verb_file = open('verbs.txt')
-kinks_file = open('tags_sex.txt')
-otags_file = open('tags_other.txt')
-chars_file = open("characters.txt")
+class File_Stuff:
+    verb_file = open('verbs.json').read()
+    kinks_file = open('tags_sex.json').read()
+    otags_file = open('tags_other.json').read()
+    chars_file = open("characters.json").read()
+    ChSpVe_file = open("char_spec_verbs.json").read() # "Character Specific Verbs", as in verbs that only apply to one character
+    verb = json.loads(verb_file)
+    kinks = json.loads(kinks_file)
+    other_tags = json.loads(otags_file)
+    character = json.loads(chars_file)
+    ChSpVe = json.loads(ChSpVe_file)
 
-verb_raw = verb_file.read()
-kinks_raw = kinks_file.read()
-otags_raw = otags_file.read()
-chars_raw = chars_file.read()
-
-verb = verb_raw.split('\n')
-kinks = kinks_raw.split('\n')
-other_tags = otags_raw.split('\n')
-character = chars_raw.split('\n')
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    userInput = message.content.lower()
-    if userInput == "slash me daddy":
-        kinkList = []
-        for tag in range(randint(3, 5)):
-            kinkList.append(choice(kinks))
-        othertaglist = []
-        for tag in range(randint(2, 4)):
-            othertaglist.append(choice(other_tags))
+@bot.command(name="slashmedaddy")
+async def randomSlash(ctx):
+    kinkList = []
+    for tag in range(randint(3, 5)):
+        kinkList.append(choice(File_Stuff.kinks))
+    othertagList = []
+    for tag in range(randint(2, 4)):
+        othertagList.append(choice(File_Stuff.other_tags))
+    
+    firstChar = choice(File_Stuff.character)
+    if File_Stuff.ChSpVe[firstChar] != [""]:
+        File_Stuff.verb.append(File_Stuff.ChSpVe[firstChar])
+    secondChar = choice(File_Stuff.character)
+    
+    response = f"{firstChar} {choice(File_Stuff.verb)} {secondChar}. Tags: {', '.join(kinkList)}, {', '.join(othertagList)}"
+    await ctx.send(response)
+    if File_Stuff.ChSpVe[firstChar] in File_Stuff.verb:
+        File_Stuff.verb.remove(File_Stuff.ChSpVe[firstChar])
         
-        response = f"{choice(character)} {choice(verb)} {choice(character)}. Tags: {', '.join(kinkList)}, {', '.join(othertaglist)}"
-        await message.channel.send(response)
-        
-client.run(TOKEN)
+bot.run(TOKEN)
